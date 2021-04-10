219af2903ef0b50b52a0504a9a48248636b3a43f
jasskarannn <59541154+jasskarannn@users.noreply.github.com>
2021-04-10 13:57:00 +0530

    second_commit

--- a.js
+++ a.js
@@ -1,1 +1,11 @@
 console.log("Hello");
+
+// git --version
+// git init
+// git remote add origin {giturl}.git  ; linking online repo 
+// git add . ; tracks all changes in files
+// git add {filename} ; adds to commit
+// git status
+// git push origin master
+// git push --set-upstream origin master; first time push access
+//git branch ; pwd i guess
